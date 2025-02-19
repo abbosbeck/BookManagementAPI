cea50f6c52b07fff7dceefb26c9c90ecd074eb18
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-19 21:46:03 +0500

    SERVICE: Implement Book View Model

---
+++ BookManagement.Core/ViewModels/BookViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BookManagement.Infrastructure.Entities;
+
+namespace BookManagement.Core.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public DateTime PublicationYear { get; set; }
+        public string AuthorName { get; set; }
+        public int ViewCount { get; set; }
+
+        public static explicit operator BookViewModel(BookEntity bookEntity)
+        {
+            return new BookViewModel
+            {
+                Title = bookEntity.Title,
+                PublicationYear = bookEntity.PublicationYear,
+                AuthorName = bookEntity.AuthorName,
+                ViewCount = bookEntity.ViewsCount
+            };
+        }
+
+        public static explicit operator BookEntity(BookViewModel bookViewModel)
+        {
+            return new BookEntity
+            {
+                Title = bookViewModel.Title,
+                PublicationYear = bookViewModel.PublicationYear,
+                AuthorName = bookViewModel.AuthorName
+            };
+        }
+    }
+}
