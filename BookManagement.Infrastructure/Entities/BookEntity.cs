2dd842dfcc3970ad1472f1c3b95f97b3fbeebafe
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-19 16:47:40 +0500

    INFRASTRUCTURE: Create Book Entity

---
+++ BookManagement.Infrastructure/Entities/BookEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BookManagement.Infrastructure.Entities
+{
+    public class BookEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PublicationYear { get; set; }
+        public string AuthorName { get; set; }
+        public int ViewsCount { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
