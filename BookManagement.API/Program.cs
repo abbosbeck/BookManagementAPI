0d31ace748ab73e5c7efd9791a5b20e967a6ce24
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-19 16:48:38 +0500

    INFRASTRUCTURE: Add Connection String & Create AppDbContext

--- BookManagement.API/Program.cs
+++ BookManagement.API/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using BookManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<AppDbContext>(options
+            => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
