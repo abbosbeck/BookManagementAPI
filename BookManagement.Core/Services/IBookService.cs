484d6d20e97510cb90ee2c4d31dad8872d205a92
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-20 15:57:29 +0500

    CODE-RUB: Fix Naming

--- BookManagement.Core/Services/IBookService.cs
+++ BookManagement.Core/Services/IBookService.cs
@@ -5,7 +5,7 @@ namespace BookManagement.Core.Services
     public interface IBookService
     {
         Task AddBookAsync(BookViewModel bookViewModel);
-        Task<(int AddedCount, List<string> SkippedTitles)> AddBooksBulkAsync(IEnumerable<BookViewModel> books);
+        Task<(int addedCount, List<string> skippedTitles)> AddBooksBulkAsync(IEnumerable<BookViewModel> books);
         Task<IEnumerable<string>> GetBookTitlesAsync(int page, int pageSize);
         Task<BookViewModel> GetBookDetailsAsync(int id);
         Task UpdateBookAsync(int id, BookViewModel bookViewModel);
