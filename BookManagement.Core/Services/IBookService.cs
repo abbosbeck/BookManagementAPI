4b5c96094fafe1dd6c357f5dd8e92c9b07c6ac43
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-22 17:00:17 +0500

    SERVICE: Add Update And Add View Book Model

--- BookManagement.Core/Services/IBookService.cs
+++ BookManagement.Core/Services/IBookService.cs
@@ -4,11 +4,11 @@ namespace BookManagement.Core.Services
 {
     public interface IBookService
     {
-        Task<int> AddBookAsync(BookViewModel bookViewModel);
-        Task<(int addedCount, List<string> skippedTitles)> AddBooksBulkAsync(IEnumerable<BookViewModel> books);
+        Task<int> AddBookAsync(BookViewForAddAndUpdateModel bookViewModel);
+        Task<(int addedCount, List<string> skippedTitles)> AddBooksBulkAsync(IEnumerable<BookViewForAddAndUpdateModel> books);
         Task<IEnumerable<string>> GetBookTitlesAsync(int page, int pageSize);
         Task<BookViewModel> GetBookDetailsAsync(int id);
-        Task UpdateBookAsync(int id, BookViewModel bookViewModel);
+        Task UpdateBookAsync(int id, BookViewForAddAndUpdateModel bookViewModel);
         Task SoftDeleteBookAsync(int id);
         Task SoftDeleteBooksBulkAsync(IEnumerable<int> bookIds);
     }
